e6af80f7c4d2cba9bd2e9f59e12b7d20d315094b
Cze Wen Goh <czewengoh@Hotmail.com>
2016-10-20 23:44:37 -0700

    Added more character and support data.

--- public/assets/json/characters.js
+++ public/assets/json/characters.js
@@ -1,10 +1,82 @@
 {
-  "Hana":{
-    "stat_mods": {"str": 1, "mag": 0, "skl_mod": 1, "spd": 2, "lck": -1, "def": -3, "res": 0},
+
+  "Avatar":{
+    "stat_mods": {"str": 0, "mag": 0, "skl": 0, "spd": 0, "lck": 0, "def": 0, "res": 0},
+    "gender": "",
+    "base_class": ["Nohr Prince(ss)" , "Hoshido Noble" , "Nohr Noble"],
+    "class": []
+  },
+
+  "Gunter":{
+    "stat_mods": {"str": 2, "mag": 0, "skl": 1, "spd": -2, "lck": 0, "def": 2, "res": -2},
+    "gender": "male",
+    "base_class": ["Cavalier" , "Paladin" , "Great Knight"],
+    "class": ["Wyvern Knight", "Wyvern Lord", "Malig Knight", "Mercenary", "Hero", "Bow Knight"]
+  },
+
+  "Felicia":{
+    "stat_mods": {"str": -2, "mag": 2, "skl": 0, "spd": 1, "lck": 0, "def": -1, "res": 1},
     "gender": "female",
-    "base_class": ["Samurai" , "Swordmaster" , "Master of Arms"],
+    "base_class": ["Troubador" , "Strategist" , "Maid"],
+    "class": ["Mercenary", "Hero", "Bow Knight"]
+  },
+
+  "Jakob":{
+    "stat_mods": {"str": 2, "mag": -2, "skl": 2, "spd": 0, "lck": -1, "def": 0, "res": -1},
+    "gender": "male",
+    "base_class": ["Troubador" , "Strategist" , "Butler"],
+    "class": ["Cavalier" , "Paladin" , "Great Knight"]
+  },
+
+  "Kaze":{
+    "stat_mods": {"str": -2, "mag": 0, "skl": 2, "spd": 3, "lck": -2, "def": -1, "res": 1},
+    "gender": "male",
+    "base_class": ["Ninja" , "Master Ninja" , "Mechanist"],
+    "class": ["Samurai", "Sword Master", "Master of Arms"]
+  },
+
+  "Azura":{
+    "stat_mods": {"str": 0, "mag": 0, "skl": 1, "spd": 3, "lck": 0, "def": -3, "res": 0},
+    "gender": "female",
+    "base_class": ["Songstress"],
+    "class": ["Sky Knight", "Kinshi Knight", "Falcon Knight"]
+  },
+
+  "Silas":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 2, "spd": 0, "lck": -1, "def": 0, "res": -1},
+    "gender": "male",
+    "base_class": ["Cavalier" , "Paladin" , "Great Knight"],
+    "class": ["Mercenary", "Hero", "Bow Knight"]
+  },
+
+  "Shura":{
+    "stat_mods": {"str": -1, "mag": 0, "skl": -1, "spd": 3, "lck": -1, "def": -2, "res": 2},
+    "gender": "male",
+    "base_class": ["Outlaw" , "Bow Knight" , "Adventurer"],
+    "class": ["Ninja", "Master Ninja", "Mechanist", "Fighter", "Hero", "Berserker"]
+  },
+
+"Izana":{
+    "stat_mods": {"str": 0, "mag": 1, "skl": 1, "spd": -2, "lck": 0, "def": 0, "res": 1},
+    "gender": "male",
+    "base_class": ["Diviner" , "Onmyoji" , "Basara"],
+    "class": ["Samurai", "Sword Master", "Master of Arms", "Apothecary", "Merchant", "Mechanist"]
+  },
+
+  "Mozu":{
+    "stat_mods": {"str": 0, "mag": 0, "skl": 1, "spd": 1, "lck": 1, "def": 0, "res": -2},
+    "gender": "male",
+    "base_class": ["Villager"],
+    "class": ["Archer", "Sniper", "Kinshi Knight"]
+  },
+
+  "Hana":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 1, "spd": 2, "lck": -1, "def": -3, "res": 0},
+    "gender": "female",
+    "base_class": ["Samurai" , "Sword Master" , "Master of Arms"],
     "class": ["Shrine Maiden", "Priestess", "Onmyoji"]
   },
+
   "Rinkah":{
     "stat_mods": {"str": -1, "mag": 0, "skl": -2, "spd": 1, "lck": 0, "def": 2, "res": 0},
     "gender": "female",
@@ -17,5 +89,237 @@
     "gender": "female",
     "base_class": ["Shrine Maiden", "Priestess", "Onmyoji" ],
     "class": ["Sky Knight", "Falcon Knight", "Kinshi Knight"]
+  },
+
+   "Subaki":{
+    "stat_mods": {"str": -1, "mag": 0, "skl": 2, "spd": -2, "lck": -1, "def": 3, "res": -1},
+    "gender": "male",
+    "base_class": ["Sky Knight", "Kinshi Knight", "Falcon Knight" ],
+    "class": ["Samurai", "Sword Master", "Master of Arms"]
+  },
+
+   "Saizo":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 3, "spd": -2, "lck": 0, "def": 1, "res": -2},
+    "gender": "male",
+    "base_class": ["Ninja", "Mechanist", "Master Ninja" ],
+    "class": ["Samurai", "Sword Master", "Master of Arms"]
+  },
+
+   "Orochi":{
+    "stat_mods": {"str": 0, "mag": 3, "skl": 2, "spd": -2, "lck": -1, "def": -2, "res": 1},
+    "gender": "female",
+    "base_class": ["Diviner", "Basara", "Onmyoji" ],
+    "class": ["Apothecary", "Merchant", "Mechanist"]
+  },
+
+   "Hinoka":{
+    "stat_mods": {"str": 1, "mag": -1, "skl": -1, "spd": 1, "lck": 0, "def": -1, "res": 2},
+    "gender": "female",
+    "base_class": ["Sky Knight", "Falcon Knight", "Kinshi Knight"],
+    "class": ["Spear Fighter", "Spear Master", "Basara"]
+  },
+
+   "Azama":{
+    "stat_mods": {"str": 2, "mag": -3, "skl": 0, "spd": 0, "lck": 0, "def": 1, "res": 0},
+    "gender": "male",
+    "base_class": ["Monk", "Great Master", "Onmyoji" ],
+    "class": ["Apothecary", "Merchant", "Mechanist"]
+  },
+
+   "Setsuna":{
+    "stat_mods": {"str": 0, "mag": 0, "skl": 1, "spd": 3, "lck": -1, "def": -1, "res": -1},
+    "gender": "female",
+    "base_class": ["Archer", "Sniper", "Kinshi Knight" ],
+    "class": ["Ninja", "Master Ninja", "Mechanist"]
+  },
+
+   "Hayato":{
+    "stat_mods": {"str": 0, "mag": 1, "skl": -1, "spd": 2, "lck": 1, "def": -1, "res": -1},
+    "gender": "male",
+    "base_class": ["Diviner", "Basara", "Onmyoji" ],
+    "class": ["Oni Savage", "Blacksmith", "Oni Chieftain"]
+  },
+
+   "Oboro":{
+    "stat_mods": {"str": 1, "mag": -1, "skl": 1, "spd": 1, "lck": -1, "def": 1, "res": -1},
+    "gender": "female",
+    "base_class": ["Spear Fighter", "Spear Master", "Basara" ],
+    "class": ["Apothecary", "Merchant", "Mechanist"]
+  },
+
+   "Hinata":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": -1, "spd": -2, "lck": 0, "def": 2, "res": 0},
+    "gender": "male",
+    "base_class": ["Samurai", "Sword Master", "Onmyoji" ],
+    "class": ["Oni Savage", "Blacksmith", "Oni Chieftain"]
+  },
+
+   "Takumi":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 3, "spd": -2, "lck": 1, "def": 0, "res": -2},
+    "gender": "male",
+    "base_class": ["Archer", "Sniper", "Kinshi Knight" ],
+    "class": ["Spear Fighter", "Spear Master", "Basara"]
+  },
+
+   "Kagero":{
+    "stat_mods": {"str": 3, "mag": 0, "skl": -1, "spd": -1, "lck": 0, "def": -1, "res": 1},
+    "gender": "female",
+    "base_class": ["Ninja", "Master Ninja", "Mechanist" ],
+    "class": ["Diviner", "Basara", "Onmyoji"]
+  },
+
+   "Reina":{
+    "stat_mods": {"str": 2, "mag": 0, "skl": 0, "spd": 2, "lck": -1, "def": -2, "res": -1},
+    "gender": "female",
+    "base_class": ["Sky Knight", "Kinshi Knight", "Falcon Knight" ],
+    "class": ["Diviner", "Basara", "Onmyoji", "Ninja", "Master Ninja", "Mechanist"]
+  },
+
+  "Kaden":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": -3, "spd": 2, "lck": 1, "def": -2, "res": 2},
+    "gender": "male",
+    "base_class": ["Kitsune", "Nine-Tails" ],
+    "class": ["Diviner", "Basara", "Onmyoji"]
+  },
+
+  "Ryoma":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 2, "spd": 1, "lck": 1, "def": -2, "res": -2},
+    "gender": "male",
+    "base_class": ["Samurai", "Sword Master", "Master of Arms" ],
+    "class": ["Sky Knight", "Falcon Knight", "Kinshi Knight"]
+  },
+
+  "Scarlet":{
+    "stat_mods": {"str": 2, "mag": 0, "skl": 0, "spd": 1, "lck": -1, "def": 1, "res": -2},
+    "gender": "female",
+    "base_class": ["Wyvern Rider", "Wyvern Lord", "Malig Knight" ],
+    "class": ["Outlaw", "Bow Knight", "Adventurer"]
+  },
+
+  "Yukimura":{
+    "stat_mods": {"str": -1, "mag": 0, "skl": 3, "spd": -1, "lck": 0, "def": -1, "res": 0},
+    "gender": "male",
+    "base_class": ["Apothecary", "Merchant", "Mechanist" ],
+    "class": ["Samurai", "Sword Master", "Master of Arms", "Monk", "Great Master", "Onmyoji"]
+  },
+
+  "Elise":{
+    "stat_mods": {"str": -1, "mag": 3, "skl": -2, "spd": 1, "lck": 1, "def": -3, "res": 1},
+    "gender": "female",
+    "base_class": ["Troubador", "Strategist", "Maid" ],
+    "class": ["Wyvern Rider", "Wyvern Lord", "Malig Knight"]
+  },
+
+  "Arthur":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 3, "spd": 0, "lck": -3, "def": 1, "res": -1},
+    "gender": "male",
+    "base_class": ["Fighter" , "Hero" , "Berserker"],
+    "class": ["Cavalier", "Paladin", "Great Knight"]
+  },
+
+  "Effie":{
+    "stat_mods": {"str": 3, "mag": 0, "skl": -1, "spd": -1, "lck": 0, "def": -1, "res": -1},
+    "gender": "female",
+    "base_class": ["Knight", "General", "Great Knight" ],
+    "class": ["Troubador", "Strategist", "Maid" ]
+  },
+
+  "Odin":{
+    "stat_mods": {"str": 0, "mag": 1, "skl": 1, "spd": -1, "lck": 1, "def": 0, "res": -1},
+    "gender": "male",
+    "base_class": ["Dark Mage", "Dark Knight", "Sorcerer" ],
+    "class": ["Samurai", "Sword Master", "Master of Arms"]
+  },
+
+  "Niles":{
+    "stat_mods": {"str": -2, "mag": 0, "skl": -1, "spd": 3, "lck": 0, "def": 0, "res": 1},
+    "gender": "male",
+    "base_class": ["Outlaw", "Bow Knight", "Adventurer" ],
+    "class": ["Dark Mage", "Dark Knight", "Sorcerer" ]
+  },
+
+  "Nyx":{
+    "stat_mods": {"str": 0, "mag": 3, "skl": -2, "spd": 2, "lck": -1, "def": -2, "res": 1},
+    "gender": "female",
+    "base_class": ["Dark Mage", "Dark Knight", "Sorcerer" ],
+    "class": ["Outlaw", "Bow Knight", "Adventurer"]
+  },
+
+  "Camilla":{
+    "stat_mods": {"str": 1, "mag": -1, "skl": 1, "spd": 1, "lck": -2, "def": 1, "res": 0},
+    "gender": "female",
+    "base_class": ["Wyvern Rider", "Wyvern Lord", "Malig Knight" ],
+    "class": ["Dark Mage", "Dark Knight", "Sorcerer" ]
+  },
+
+  "Selena":{
+    "stat_mods": {"str": -1, "mag": 0, "skl": -1, "spd": 2, "lck": 0, "def": 1, "res": 0},
+    "gender": "female",
+    "base_class": ["Mercenary", "Hero", "Bow Knight" ],
+    "class": ["Sky Knight", "Kinshi Knight", "Falcon Knight"]
+  },
+
+  "Beruka":{
+    "stat_mods": {"str": -1, "mag": 0, "skl": 2, "spd": -2, "lck": 0, "def": 2, "res": -1},
+    "gender": "female",
+    "base_class": ["Wyvern Rider", "Wyvern Lord", "Malig Knight" ],
+    "class": ["Fighter", "Hero", "Berserker"]
+  },
+
+  "Laslow":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": 2, "spd": -1, "lck": 1, "def": -1, "res": -1},
+    "gender": "male",
+    "base_class": ["Mercenary", "Hero", "Bow Knight" ],
+    "class": ["Ninja", "Master Ninja", "Mechanist"]
+  },
+
+  "Peri":{
+    "stat_mods": {"str": 1, "mag": 0, "skl": -1, "spd": 1, "lck": 0, "def": -2, "res": 2},
+    "gender": "female",
+    "base_class": ["Cavalier", "Paladin", "Great Knight" ],
+    "class": ["Dark Mage", "Dark Knight", "Sorcerer" ]
+  },
+
+  "Benny":{
+    "stat_mods": {"str": 0, "mag": 0, "skl": 0, "spd": -3, "lck": 0, "def": 3, "res": 1},
+    "gender": "male",
+    "base_class": ["Knight", "General", "Great Knight" ],
+    "class": ["Fighter", "Hero", "Berserker"]
+  },
+
+  "Charlotte":{
+    "stat_mods": {"str": 3, "mag": 0, "skl": 0, "spd": 2, "lck": 0, "def": -2, "res": -2},
+    "gender": "female",
+    "base_class": ["Fighter", "Hero", "Berserker" ],
+    "class": ["Troubador", "Strategist", "Maid"]
+  },
+
+  "Leo":{
+    "stat_mods": {"str": -2, "mag": 2, "skl": 0, "spd": -2, "lck": 0, "def": 0, "res": 2},
+    "gender": "male",
+    "base_class": ["Dark Mage", "Dark Knight", "Sorcerer" ],
+    "class": ["Troubador", "Strategist", "Butler"]
+  },
+
+  "Keaton":{
+    "stat_mods": {"str": 3, "mag": 0, "skl": -2, "spd": -1, "lck": 0, "def": 2, "res": -1},
+    "gender": "male",
+    "base_class": ["Wolfskin", "Wolfssegner "],
+    "class": ["Fighter", "Hero", "Berserker"]
+  },
+
+  "Xander":{
+    "stat_mods": {"str": 2, "mag": -1, "skl": -1, "spd": -1, "lck": 2, "def": 1, "res": -2},
+    "gender": "male",
+    "base_class": ["Cavalier", "Paladin", "Great Knight" ],
+    "class": ["Wyvern Rider", "Wyvern Lord", "Malig Knight" ]
+  },
+
+  "Flora":{
+    "stat_mods": {"str": 1, "mag": -1, "skl": 2, "spd": 0, "lck": -1, "def": 1, "res": -1},
+    "gender": "female",
+    "base_class": ["Troubador", "Strategist", "Maid" ],
+    "class": ["Dark Mage", "Dark Knight", "Sorcerer", "Mercenary", "Hero", "Bow Knight"]
   }
+
 }
--- public/assets/json/supports.js
+++ public/assets/json/supports.js
@@ -23,12 +23,379 @@
 
   "Sakura":{
     "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki"],
-    "nohr": ["Cavalier", "Leo"],
+    "nohr": ["Xander", "Leo"],
     "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
     "A+_support": {
       "hoshido": ["Hana", "Hinoka"],
       "nohr": ["Elise"],
       "both_routes": ["Azura"]
     }
+  },
+
+
+   "Subaki":{
+      "hoshido": ["Hinoka", "Sakura", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Hinata", "Saizo", "Azama"],
+        "nohr": ["Niles"],
+        "both_routes": []
+      }
+  },
+
+   "Saizo":{
+     "hoshido": ["Hinoka", "Sakura", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Charlotte", "Beruka"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Ryoma", "Subaki"],
+        "nohr": ["Laslow"],
+        "both_routes": ["Kaze"]
+      }
+  },
+
+  "Orochi":{
+     "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki", "Ryoma", "Takumi"],
+      "nohr": ["Laslow", "Odin"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Rinkah", "Kagero", "Oboro"],
+        "nohr": ["Nyx"],
+        "both_routes": []
+      }
+  },
+
+  "Hinoka":{
+      "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki"],
+      "nohr": ["Xander", "Leo"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Sakura", "Setsuna"],
+        "nohr": ["Camilla"],
+        "both_routes": ["Azura"]
+      }
+  },
+
+   "Azama":{
+      "hoshido": ["Hinoka", "Sakura", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Effie", "Beruka"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Kaden", "Subaki", "Hayato"],
+        "nohr": ["Arthur"],
+        "both_routes": []
+      }
+  },
+
+   "Setsuna":{
+      "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki", "Ryoma", "Takumi"],
+      "nohr": ["Niles", "Arthur"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Hinoka", "Kagero", "Hana"],
+        "nohr": ["Selena"],
+        "both_routes": []
+      }
+  },
+
+  "Hayato":{
+    "hoshido": ["Hinoka", "Sakura", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Nyx", "Effie"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Kaden", "Azama"],
+        "nohr": ["Benny"],
+        "revelations": ["Fuga"],
+        "both_routes": []
+      }
+  },
+
+  "Oboro":{
+      "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki", "Ryoma", "Takumi"],
+      "nohr": ["Niles", "Benny"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Orochi", "Rinkah", "Mozu"],
+        "nohr": ["Beruka"],
+        "both_routes": []
+      }
+  },
+
+  "Hinata":{
+      "hoshido": ["Hinoka", "Sakura", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Peri", "Selena"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Takumi", "Kaden", "Subaki"],
+        "nohr": ["Odin"],
+        "both_routes": []
+      }
+  },
+
+  "Takumi":{
+    "hoshido": ["Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Camilla", "Elise"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Ryoma", "Hinata", "Jakob"],
+        "nohr": ["Leo"],
+        "both_routes": []
+      }
+  },
+
+  "Kagero":{
+    "hoshido": ["Saizo", "Hinata", "Hayato", "Kaden", "Azama", "Subaki", "Ryoma", "Takumi"],
+      "nohr": ["Odin", "Arthur"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Orochi", "Rinkah", "Setsuna"],
+        "nohr": ["Peri"],
+        "both_routes": []
+      }
+  },
+
+  "Reina":{
+    "hoshido": [],
+      "nohr": [],
+      "both_routes": ["Avatar (M)"],
+      "A+_support": {
+        "hoshido": [],
+        "nohr": [],
+        "both_routes": []
+      }
+  },
+
+  "Kaden":{
+      "hoshido": ["Sakura", "Hinoka", "Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Charlotte", "Peri"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Hinata", "Azama", "Hayato"],
+        "nohr": ["Keaton"],
+        "both_routes": []
+      }
+  },
+
+  "Ryoma":{
+      "hoshido": ["Rinkah", "Orochi", "Kagero", "Hana", "Setsuna", "Oboro"],
+      "nohr": ["Camilla", "Elise"],
+      "both_routes": ["Avatar (F)", "Azura", "Felicia", "Mozu"],
+      "A+_support": {
+        "hoshido": ["Takumi", "Saizo", "Silas"],
+        "nohr": ["Xander"],
+        "both_routes": []
+      }
+  },
+
+  "Scarlet":{
+      "hoshido": [],
+      "nohr": [],
+      "both_routes": ["Avatar (M)"],
+      "A+_support": {
+        "hoshido": [],
+        "nohr": [],
+        "both_routes": []
+      }
+  },
+
+  "Yukimura":{
+    "hoshido": [],
+      "nohr": [],
+      "both_routes": ["Avatar (F)"],
+      "A+_support": {
+        "hoshido": [],
+        "nohr": [],
+        "both_routes": []
+      }
+  },
+
+  "Elise":{
+      "hoshido": ["Ryoma", "Takumi"],
+      "nohr": ["Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Sakura"],
+        "nohr": ["Camilla", "Effie"],
+        "both_routes": []
+      }
+  },
+
+  "Arthur":{
+      "hoshido": ["Kagero", "Setsuna"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Azama"],
+        "nohr": ["Benny", "Keaton", "Niles"],
+        "both_routes": []
+      }
+  },
+
+  "Effie":{
+      "hoshido": ["Hayato", "Azama"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Hana"],
+        "nohr": ["Elise", "Nyx"],
+        "both_routes": ["Mozu"]
+      }
+  },
+
+  "Odin":{
+      "hoshido": ["Orochi", "Kagero"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Hinata"],
+        "nohr": ["Leo", "Laslow", "Niles"],
+        "both_routes": []
+      }
+  },
+
+  "Niles":{
+      "hoshido": ["Setsuna", "Oboro"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Subaki"],
+        "nohr": ["Leo", "Odin", "Arthur"],
+        "both_routes": []
+      }
+  },
+
+  "Nyx":{
+      "hoshido": ["Hayato", "Subaki"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Orochi"],
+        "nohr": ["Charlotte", "Effie"],
+        "both_routes": ["Mozu"]
+      }
+  },
+
+  "Camilla":{
+      "hoshido": ["Ryoma", "Takumi"],
+      "nohr": ["Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Hinoka"],
+        "nohr": ["Elise", "Beruka", "Selena"],
+        "both_routes": ["Azura"]
+      }
+  },
+
+  "Selena":{
+      "hoshido": ["Subaki", "Hinata"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Setsuna"],
+        "nohr": ["Camilla", "Beruka", "Peri"],
+        "both_routes": []
+      }
+  },
+
+  "Beruka":{
+      "hoshido": ["Azama", "Saizo"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Oboro"],
+        "nohr": ["Camilla", "Charlotte", "Selena"],
+        "both_routes": []
+      }
+  },
+
+  "Laslow":{
+      "hoshido": ["Hana", "Orochi"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Saizo"],
+        "nohr": ["Xander", "Odin", "Keaton"],
+        "both_routes": []
+      }
+  },
+
+  "Peri":{
+    "hoshido": ["Hinata", "Kaden"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Kagero"],
+        "nohr": ["Charlotte", "Selena"],
+        "both_routes": ["Felicia"]
+      }
+  },
+
+  "Benny":{
+      "hoshido": ["Rinkah", "Oboro"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Hayato"],
+        "nohr": ["Keaton", "Arthur"],
+        "both_routes": []
+      }
+  },
+
+  "Charlotte":{
+      "hoshido": ["Saizo", "Kaden"],
+      "nohr": ["Xander", "Leo", "Benny", "Keaton", "Arthur", "Odin", "Laslow", "Niles"],
+      "both_routes": ["Avatar (M)", "Jakob", "Kaze", "Silas"],
+      "A+_support": {
+        "hoshido": ["Rinkah"],
+        "nohr": ["Peri", "Beruka", "Nyx"],
+        "both_routes": []
+      }
+  },
+
+  "Leo":{
+      "hoshido": ["Hinoka", "Saskura"],
+      "nohr": ["Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Takumi"],
+        "nohr": ["Xander", "Odin", "Niles"],
+        "both_routes": []
+      }
+  },
+
+  "Keaton":{
+      "hoshido": ["Rinkah", "Hanas"],
+      "nohr": ["Camilla", "Elise", "Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Kaden"],
+        "nohr": ["Benny", "Arthur", "Laslow"],
+        "both_routes": []
+      }
+  },
+
+  "Xander":{
+      "hoshido": ["Hinoka", "Saskura"],
+      "nohr": ["Charlotte", "Effie", "Peri", "Beruka", "Selena", "Nyx"],
+      "both_routes": ["Avatar (F)", "Felicia", "Mozu", "Azura"],
+      "A+_support": {
+        "hoshido": ["Ryoma"],
+        "nohr": ["Leo", "Laslow"],
+        "both_routes": ["Kaze"]
+      }
+  },
+
+  "Flora":{
+      "hoshido": [],
+      "nohr": [],
+      "both_routes": ["Avatar (M)"],
+      "A+_support": {
+        "hoshido": [],
+        "nohr": [],
+        "both_routes": ["Felicia"]
+      }
   }
+
+
 }
